85d0318c5160f374fc88f8b56b8a2a9d6e991be8
ESSO0428 <ESSO0428@gmail.com>
2024-09-02 17:14:53 +0800

    feat(nvim): add transparent background highlights
    Added configuration to set transparent backgrounds for various highlights
    in Neovim. This change improves the visual appearance by making certain
    elements transparent.

--- VimKeyMap/nvim/lua/opt.lua
+++ VimKeyMap/nvim/lua/opt.lua
@@ -6,3 +6,21 @@ vim.cmd [[
 		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 		set shellquote= shellxquote=
 ]]
+
+local transparent_highlights = {
+  'Normal',
+  'NormalNC',
+  'LineNr',
+  'Folded',
+  'NonText',
+  'SpecialKey',
+  'VertSplit',
+  'SignColumn',
+  'EndOfBuffer',
+  'TablineFill', -- this might be preference
+}
+
+
+for _, hl in ipairs(transparent_highlights) do
+  vim.cmd.highlight(hl .. ' guibg=NONE ctermbg=NONE')
+end
