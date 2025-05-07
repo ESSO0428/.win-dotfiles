e0d3f8cd03744d4522d02a21f23a96ae123041e3
ESSO0428 <ESSO0428@gmail.com>
2025-05-07 22:25:02 +0800

    feat(cmp): enhance nvim-cmp with additional sources and features
    - Add `buffer` completion source with size limitation
    - Implement jumpable snippet navigation
    - Add multiple formatting options for completion items
    - Configure better mapping for navigating completions
    - Add ghost text experimental feature
    - Improve display of source names in completion menu

--- VimKeyMap/nvim/lua/user/plugins.lua
+++ VimKeyMap/nvim/lua/user/plugins.lua
@@ -896,13 +896,150 @@ require("lazy").setup({
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
     },
     config = function()
       -- See `:help cmp`
       local cmp = require "cmp"
+      local cmp_mapping = require "cmp.config.mapping"
+      local status_cmp_ok, cmp_types = pcall(require, "cmp.types.cmp")
+      if not status_cmp_ok then
+        return
+      end
+      local ConfirmBehavior = cmp_types.ConfirmBehavior
+      local SelectBehavior = cmp_types.SelectBehavior
       local luasnip = require "luasnip"
       luasnip.config.setup {}
+
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      ---when inside a snippet, seeks to the nearest luasnip field if possible, and checks if it is jumpable
+      ---@param dir number 1 for forward, -1 for backward; defaults to 1
+      ---@return boolean true if a jumpable luasnip field is found while inside a snippet
+      local function jumpable(dir)
+        local win_get_cursor = vim.api.nvim_win_get_cursor
+        local get_current_buf = vim.api.nvim_get_current_buf
+
+        ---sets the current buffer's luasnip to the one nearest the cursor
+        ---@return boolean true if a node is found, false otherwise
+        local function seek_luasnip_cursor_node()
+          -- TODO(kylo252): upstream this
+          -- for outdated versions of luasnip
+          if not luasnip.session.current_nodes then
+            return false
+          end
+
+          local node = luasnip.session.current_nodes[get_current_buf()]
+          if not node then
+            return false
+          end
+
+          local snippet = node.parent.snippet
+          local exit_node = snippet.insert_nodes[0]
+
+          local pos = win_get_cursor(0)
+          pos[1] = pos[1] - 1
+
+          -- exit early if we're past the exit node
+          if exit_node then
+            local exit_pos_end = exit_node.mark:pos_end()
+            if (pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2]) then
+              snippet:remove_from_jumplist()
+              luasnip.session.current_nodes[get_current_buf()] = nil
+
+              return false
+            end
+          end
+
+          node = snippet.inner_first:jump_into(1, true)
+          while node ~= nil and node.next ~= nil and node ~= snippet do
+            local n_next = node.next
+            local next_pos = n_next and n_next.mark:pos_begin()
+            local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
+              or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
+
+            -- Past unmarked exit node, exit early
+            if n_next == nil or n_next == snippet.next then
+              snippet:remove_from_jumplist()
+              luasnip.session.current_nodes[get_current_buf()] = nil
+
+              return false
+            end
+
+            if candidate then
+              luasnip.session.current_nodes[get_current_buf()] = node
+              return true
+            end
+
+            local ok
+            ok, node = pcall(node.jump_from, node, 1, true) -- no_move until last stop
+            if not ok then
+              snippet:remove_from_jumplist()
+              luasnip.session.current_nodes[get_current_buf()] = nil
+
+              return false
+            end
+          end
+
+          -- No candidate, but have an exit node
+          if exit_node then
+            -- to jump to the exit node, seek to snippet
+            luasnip.session.current_nodes[get_current_buf()] = snippet
+            return true
+          end
+
+          -- No exit node, exit from snippet
+          snippet:remove_from_jumplist()
+          luasnip.session.current_nodes[get_current_buf()] = nil
+          return false
+        end
+
+        if dir == -1 then
+          return luasnip.in_snippet() and luasnip.jumpable(-1)
+        else
+          return luasnip.in_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable(1)
+        end
+      end
+      local cmp_confirm_opts = {
+        behavior = ConfirmBehavior.Replace,
+        select = false,
+      }
+      local cmp_formatting_source_names = {
+        buffer = "(Buffer)",
+        calc = "(Calc)",
+        cmp_tabnine = "(Tabnine)",
+        copilot = "(Copilot)",
+        ["copilot-chat"] = "(copilot-chat)",
+        dap = "(dap)",
+        emoji = "(Emoji)",
+        ["html-css"] = "(html-css)",
+        jupynium = "(jupynium)",
+        jupyter = "(jupyter)",
+        lazydev = "(lazydev)",
+        luasnip = "(L-Snippet)",
+        nvim_lsp = "(LSP)",
+        orgmode = "(orgmode)",
+        path = "(Path)",
+        tmux = "(TMUX)",
+        treesitter = "(TreeSitter)",
+        ultisnips = "(U-Snippet)",
+        ["vim-dadbod-completion"] = "(dadbod-sql)",
+        vsnip = "(V-Snippet)",
+      }
+      local cmp_formatting_duplicates = {
+        ["html-css"] = 1,
+        buffer = 1,
+        path = 1,
+        nvim_lsp = 1,
+        luasnip = 1,
+        ultisnips = 1,
+        vsnip = 1,
+      }
+      local cmp_duplicates_default = 0
 
       cmp.setup {
         window = {
@@ -911,71 +1048,195 @@ require("lazy").setup({
             winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
           },
         },
+        -- completion = { completeopt = "menu,menuone,noinsert" },
+        completion = {
+          ---@usage The minimum length of a word to complete on.
+          keyword_length = 1,
+        },
+        experimental = {
+          ghost_text = true,
+          native_menu = false,
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            local max_width = 0
+            if max_width ~= 0 and #vim_item.abbr > max_width then
+              vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. require("user.config.icons").ui.Ellipsis
+            end
+
+            -- tailwindcss color preview
+            --[[
+            local doc = entry.completion_item.documentation
+            if vim_item.kind == "Color" and doc then
+              local utils = require "tailwind-tools.utils"
+              local content = type(doc) == "string" and doc or doc.value
+              local base, _, _, _r, _g, _b = 10, content:find "rgba?%((%d+), (%d+), (%d+)"
+
+              if not _r then
+                base, _, _, _r, _g, _b = 16, content:find "#(%x%x)(%x%x)(%x%x)"
+              end
+
+              if _r then
+                local r, g, b = tonumber(_r, base), tonumber(_g, base), tonumber(_b, base)
+                vim_item.kind_hl_group = utils.set_hl_from(r, g, b, "foreground")
+              end
+            end
+            --]]
+
+            vim_item.kind = require("user.config.icons").kind[vim_item.kind]
+
+            if entry.source.name == "copilot" then
+              if vim.bo.filetype == "prompt" then
+                return
+              end
+              vim_item.kind = require("user.config.icons").git.Octoface
+              vim_item.kind_hl_group = "CmpItemKindCopilot"
+            end
+            if entry.source.name == "copilot-chat" then
+              vim_item.kind = require("user.config.icons").git.Octoface
+              vim_item.kind_hl_group = "CmpItemKindCopilot"
+            end
+            if entry.source.name == "cmp_tabnine" then
+              vim_item.kind = require("user.config.icons").misc.Robot
+              vim_item.kind_hl_group = "CmpItemKindTabnine"
+            end
+
+            if entry.source.name == "crates" then
+              vim_item.kind = require("user.config.icons").misc.Package
+              vim_item.kind_hl_group = "CmpItemKindCrate"
+            end
+
+            if entry.source.name == "lab.quick_data" then
+              vim_item.kind = require("user.config.icons").misc.CircuitBoard
+              vim_item.kind_hl_group = "CmpItemKindConstant"
+            end
+
+            if entry.source.name == "emoji" then
+              vim_item.kind = require("user.config.icons").misc.Smiley
+              vim_item.kind_hl_group = "CmpItemKindEmoji"
+            end
+
+            if entry.source.name == "orgmode" then
+              -- vim_item.kind = ""
+              -- vim_item.kind_hl_group = "CmpItemKindCopilot"
+              local ok, devicons = pcall(require, "nvim-web-devicons")
+              if ok then
+                local icon, icon_highlight_group = devicons.get_icon(vim.fn.expand "%:t")
+                if icon == nil then
+                  icon, icon_highlight_group = devicons.get_icon_by_filetype(vim.bo.filetype)
+                end
+                vim_item.kind = icon
+                vim_item.kind_hl_group = icon_highlight_group
+              end
+            end
+            if entry.source.name == "vim-dadbod-completion" then
+              vim_item.kind = ""
+              vim_item.kind_hl_group = "CmpItemKindEmoji"
+            end
+            vim_item.menu = cmp_formatting_source_names[entry.source.name]
+            vim_item.dup = cmp_formatting_duplicates[entry.source.name] or cmp_duplicates_default
+
+            if entry.source.name == "html-css" then
+              -- vim_item.menu = entry.source.menu
+              vim_item.menu = "(html-css)" .. " " .. entry.completion_item.menu
+            end
+            return vim_item
+          end,
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = "menu,menuone,noinsert" },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          -- ["<a-k>"] = cmp.mapping.select_next_item(),
-          ["<a-k>"] = cmp.mapping(function(fallback)
+        mapping = cmp_mapping.preset.insert {
+          -- ["<M-i>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
+          ["<M-i>"] = cmp_mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
+              -- cmp.select_prev_item()
+              cmp.select_prev_item { behavior = SelectBehavior.Select }
             else
               cmp.complete()
+              --fallback()
             end
-          end, { "i", "s" }),
-          -- Select the [p]revious item
-          ["<a-i>"] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-o>"] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ["<a-l>"] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          -- ["<c-space>"] = cmp.mapping.complete({}),
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ["<a-d>"] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
+          end, { "i", "c" }),
+          ["<M-k>"] = cmp_mapping(function(fallback)
+            if cmp.visible() then
+              -- cmp.select_next_item()
+              cmp.select_next_item { behavior = SelectBehavior.Select }
+            else
+              cmp.complete()
+              --fallback()
+            end
+          end, { "i", "c" }),
+          ["<M-j>"] = cmp.mapping(cmp_mapping.abort(), { "i", "c" }),
+          -- ["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
+          ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item { behavior = SelectBehavior.Select }, { "i" }),
+          ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item { behavior = SelectBehavior.Select }, { "i" }),
+          ["<C-u>"] = cmp_mapping.scroll_docs(-4),
+          ["<C-o>"] = cmp_mapping.scroll_docs(4),
+          ["<M-l>"] = cmp_mapping {
+            -- i = cmp_mapping.confirm { behavior = ConfirmBehavior.Replace, select = false },
+            i = cmp_mapping.confirm { cmp_confirm_opts },
+            c = function(fallback)
+              if cmp.visible() then
+                cmp.confirm { behavior = ConfirmBehavior.Replace, select = false }
+              else
+                fallback()
+              end
+            end,
+          },
+          ["<M-d>"] = cmp_mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
+            elseif jumpable(1) then
+              luasnip.jump(1)
+            elseif has_words_before() then
+              -- cmp.complete()
+              fallback()
+            else
+              fallback()
             end
           end, { "i", "s" }),
-          ["<a-a>"] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
+          ["<M-a>"] = cmp_mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
               luasnip.jump(-1)
+            else
+              fallback()
             end
           end, { "i", "s" }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+          -- ["<C-Space>"] = cmp_mapping.complete(),
+          -- ["<C-e>"] = cmp_mapping.abort(),
+          ["<CR>"] = cmp_mapping(function(fallback)
+            if cmp.visible() then
+              local confirm_opts = vim.deepcopy(lvim.builtin.cmp.confirm_opts) -- avoid mutating the original opts below
+              local is_insert_mode = function()
+                return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
+              end
+              if is_insert_mode() then -- prevent overwriting brackets
+                confirm_opts.behavior = ConfirmBehavior.Insert
+              end
+              local entry = cmp.get_selected_entry()
+              local is_copilot = entry and entry.source.name == "copilot"
+              -- if is_copilot then
+              --   confirm_opts.behavior = ConfirmBehavior.Replace
+              --   confirm_opts.select = true
+              -- end
+              if cmp.confirm(confirm_opts) then
+                return -- success, exit early
+              end
+            end
+            fallback() -- if not exited early, always fallback
+          end),
         },
         sources = {
           {
@@ -986,6 +1247,32 @@ require("lazy").setup({
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "path" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                local max_size = 100000 -- 设置文件大小限制为 100,000 字节
+                local bufs = {}
+
+                -- 获取当前 Tab 中的所有窗口
+                local windows = vim.api.nvim_tabpage_list_wins(0)
+                for _, win in ipairs(windows) do
+                  -- 获取每个窗口的缓冲区编号
+                  local buf = vim.api.nvim_win_get_buf(win)
+                  -- 检查文件类型是否不是 neo-tree
+                  if vim.api.nvim_get_option_value("filetype", { buf = buf }) ~= "neo-tree" then
+                    -- 检查文件大小
+                    local size = vim.fn.getfsize(vim.api.nvim_buf_get_name(buf))
+                    if size > 0 and size < max_size then
+                      bufs[buf] = true
+                    end
+                  end
+                end
+
+                return vim.tbl_keys(bufs)
+              end,
+            },
+          },
         },
       }
     end,
