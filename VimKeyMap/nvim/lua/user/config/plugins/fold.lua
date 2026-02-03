9d7ce576844355cd5e0328d04d6e2c8e0ae708f5
Andy6 <ESSO028@gmail.com>
2026-02-03 15:30:11 +0800

    perf(config): modularize plugin configurations
    Convert multiple plugin configuration files to use module pattern
    with setup() functions. This enables lazy-loading of configuration
    code and improves startup performance.
    
    Changes:
    - bigfile.lua: Wrap config in module with setup() function
    - MarkdownNvim.lua: Extract inline config to separate module
    - fold.lua: Convert to module pattern and add setup() function
    - headline.lua: Wrap config in module with setup() function
    - hydra.lua: Convert to module pattern with setup() function
    - plugins.lua: Update all plugin specs to call respective setup()
      functions
    
    This refactoring defers configuration execution until plugins are
    actually loaded, reducing initial startup overhead.

--- VimKeyMap/nvim/lua/user/config/plugins/fold.lua
+++ VimKeyMap/nvim/lua/user/config/plugins/fold.lua
@@ -1,3 +1,4 @@
+local M = {}
 -- ufo folding
 vim.opt.foldcolumn = "1"
 vim.o.foldlevel = 99
@@ -12,93 +13,106 @@ vim.o.foldmethod = "manual"
 -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-  require("lspconfig")[ls].setup {
-    capabilities = capabilities,
-    -- you can add other fields for setting up lsp server in this table
+function M.setup()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
   }
-end
-local ftMap = {
-  vim = "indent",
-  python = { "indent" },
-  git = "",
-}
-local handler = function(virtText, lnum, endLnum, width, truncate)
-  local newVirtText = {}
-  local suffix = (" … 󱦶 %d "):format(endLnum - lnum)
-  local sufWidth = vim.fn.strdisplaywidth(suffix)
-  local targetWidth = width - sufWidth
-  local curWidth = 0
-  for _, chunk in ipairs(virtText) do
-    local chunkText = chunk[1]
-    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    if targetWidth > curWidth + chunkWidth then
-      table.insert(newVirtText, chunk)
-    else
-      chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk[2]
-      table.insert(newVirtText, { chunkText, hlGroup })
-      chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
-      if curWidth + chunkWidth < targetWidth then
-        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-      end
-      break
-    end
-    curWidth = curWidth + chunkWidth
-  end
-  table.insert(newVirtText, { suffix, "MoreMsg" })
-  return newVirtText
-end
-require("ufo").setup {
-  fold_virt_text_handler = handler,
-  provider_selector = function(bufnr, filetype, buftype)
-    local empty_return_filetypes = {
-      "org",
-      "markdown",
-      "copilot-chat",
-      "Avante",
+  local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+  for _, ls in ipairs(language_servers) do
+    require("lspconfig")[ls].setup {
+      capabilities = capabilities,
+      -- you can add other fields for setting up lsp server in this table
     }
-    if vim.tbl_contains(empty_return_filetypes, filetype) then
-      return ""
-    end
-    -- if you prefer treesitter provider rather than lsp,
-    -- return ftMap[filetype] or {'treesitter', 'indent'}
-    return ftMap[filetype]
-  end,
-  preview = {
-    mappings = {
-      scrollU = "<C-u>",
-      scrollD = "<C-o>",
-      jumpTop = "gg",
-      jumpBot = "G",
-      switch = "u",
-    },
-  },
-}
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    if vim.api.nvim_buf_get_name(0):match "UfoPreviewFloatWin" then
-      vim.opt_local.list = false
-      vim.cmd "UfoAttach"
-      vim.cmd "UfoEnableFold"
-      vim.cmd "normal! zX"
-    else
-      if vim.wo.foldenable then
-        vim.cmd "UfoEnableFold"
+  end
+  local ftMap = {
+    vim = "indent",
+    python = { "indent" },
+    git = "",
+  }
+  local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (" … 󱦶 %d "):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+      local chunkText = chunk[1]
+      local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      if targetWidth > curWidth + chunkWidth then
+        table.insert(newVirtText, chunk)
+      else
+        chunkText = truncate(chunkText, targetWidth - curWidth)
+        local hlGroup = chunk[2]
+        table.insert(newVirtText, { chunkText, hlGroup })
+        chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        -- str width returned from truncate() may less than 2nd argument, need padding
+        if curWidth + chunkWidth < targetWidth then
+          suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+        end
+        break
       end
+      curWidth = curWidth + chunkWidth
     end
-  end,
-})
+    table.insert(newVirtText, { suffix, "MoreMsg" })
+    return newVirtText
+  end
+  require("ufo").setup {
+    fold_virt_text_handler = M.handler,
+    provider_selector = function(bufnr, filetype, buftype)
+      local empty_return_filetypes = {
+        "org",
+        "markdown",
+        "copilot-chat",
+        "Avante",
+      }
+      if vim.tbl_contains(empty_return_filetypes, filetype) then
+        return ""
+      end
+      -- if you prefer treesitter provider rather than lsp,
+      -- return ftMap[filetype] or {'treesitter', 'indent'}
+      return ftMap[filetype]
+    end,
+    preview = {
+      mappings = {
+        scrollU = "<C-u>",
+        scrollD = "<C-o>",
+        jumpTop = "gg",
+        jumpBot = "G",
+        switch = "u",
+      },
+    },
+  }
 
+  vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    callback = function()
+      if vim.api.nvim_buf_get_name(0):match "UfoPreviewFloatWin" then
+        vim.opt_local.list = false
+        vim.cmd "UfoAttach"
+        vim.cmd "UfoEnableFold"
+        vim.cmd "normal! zX"
+      else
+        if vim.wo.foldenable then
+          vim.cmd "UfoEnableFold"
+        end
+      end
+    end,
+  })
+
+  -- global handler
+  -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+  -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+  -- require('ufo').setup({
+  --   fold_virt_text_handler = M.handler
+  -- })
+
+  -- buffer scope handler
+  -- will override global handler if it is existed
+  -- local bufnr = vim.api.nvim_get_current_buf()
+  -- require('ufo').setFoldVirtTextHandler(bufnr, handler)
+end
 function PeekFoldedLinesUnderCursor()
   require("ufo").peekFoldedLinesUnderCursor()
   local winid = require("ufo.preview.floatwin").winid
@@ -130,14 +144,4 @@ vim.keymap.set("n", "]7", "<cmd>lua Vscode_like_foldLevel_enhance(7)<cr>")
 vim.keymap.set("n", "]8", "<cmd>lua Vscode_like_foldLevel_enhance(8)<cr>")
 vim.keymap.set("n", "]9", "<cmd>lua Vscode_like_foldLevel_enhance(9)<cr>")
 
--- global handler
--- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
--- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
--- require('ufo').setup({
---   fold_virt_text_handler = handler
--- })
-
--- buffer scope handler
--- will override global handler if it is existed
--- local bufnr = vim.api.nvim_get_current_buf()
--- require('ufo').setFoldVirtTextHandler(bufnr, handler)
+return M
--- VimKeyMap/nvim/lua/user/plugins.lua
+++ VimKeyMap/nvim/lua/user/plugins.lua
@@ -61,7 +61,12 @@ require("lazy").setup({
       }
     },
   },
-  { "LunarVim/bigfile.nvim" },
+  {
+    "LunarVim/bigfile.nvim",
+    config = function()
+      require("user.bigfile").setup()
+    end,
+  },
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
@@ -1971,6 +1976,9 @@ require("lazy").setup({
     "kevinhwang91/nvim-ufo",
     event = "BufReadPost", -- 打開檔案後再載
     deprecated = { "kevinhwang91/promise-async" },
+    config = function()
+      require("user.config.plugins.fold").setup()
+    end,
   },
   {
     "luukvbaal/statuscol.nvim",
@@ -2016,7 +2024,9 @@ require("lazy").setup({
     "lukas-reineke/headlines.nvim",
     ft = { "markdown", "norg", "org" }, -- ★ 比 VeryLazy 更精準
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true, -- or `opts = {}`
+    config = function()
+      require("user.config.plugins.headline").setup()
+    end,
   },
   -- NOTE: The handling of `concealcursor` in LSP hover markdown rendering
   -- was changed after commit 0022a57. The previous behavior allowed
@@ -2035,116 +2045,7 @@ require("lazy").setup({
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     config = function()
-      vim.g.MarkdownNvim = 1
-      vim.treesitter.language.register("markdown", "copilot-chat")
-      vim.treesitter.language.register("markdown", "AvanteInput")
-      require("render-markdown").setup {
-        file_types = { "markdown", "copilot-chat", "Avante", "AvanteInput" },
-        overrides = {
-          buftype = {
-            nofile = {
-              win_options = {
-                conceallevel = {
-                  default = 0,
-                  rendered = 2,
-                },
-                concealcursor = {
-                  default = "nvic",
-                  rendered = "nvic",
-                },
-              },
-            },
-          },
-        },
-        heading = {
-          sign = false,
-          icons = { " ◉ ", " ○ ", " ✸ ", " ✿ ", " ◉ ", " ○ " },
-        },
-        quote = {
-          -- Turn on / off block quote & callout rendering
-          enabled = true,
-          -- Replaces '>' of 'block_quote'
-          icon = "▋",
-          -- Highlight for the quote icon
-          highlight = "RenderMarkdownQuote",
-        },
-        code = {
-          sign = false,
-          border = "thick",
-          highlight = "RenderMarkdownCode",
-          highlight_inline = "",
-        },
-        bullet = {
-          icons = { "●", "○", "◆", "◇" },
-          -- Padding to add to the right of bullet point
-          right_pad = 0,
-          -- Highlight for the bullet icon
-          -- highlight = 'RenderMarkdownBullet',
-          highlight = "Identifier",
-        },
-        html = {
-          -- Turn on / off all HTML rendering
-          enabled = true,
-          comment = {
-            -- Turn on / off HTML comment concealing
-            conceal = false,
-            -- Optional text to inline before the concealed comment
-            text = nil,
-            -- Highlight for the inlined text
-            highlight = "RenderMarkdownHtmlComment",
-          },
-        },
-        win_options = {
-          -- See :h 'conceallevel'
-          conceallevel = {
-            -- Used when not being rendered, get user setting
-            default = 0,
-            -- Used when being rendered, concealed text is completely hidden
-            rendered = 2,
-          },
-        },
-        link = {
-          -- Turn on / off inline link icon rendering
-          enabled = true,
-          -- Inlined with 'image' elements
-          image = "󰥶 ",
-          -- Inlined with 'email_autolink' elements
-          email = "󰀓 ",
-          -- Fallback icon for 'inline_link' elements
-          hyperlink = "󰌹 ",
-          -- Applies to the fallback inlined icon
-          highlight = "RenderMarkdownLink",
-          -- Applies to WikiLink elements
-          wiki = { icon = "󱗖 ", highlight = "RenderMarkdownWikiLink" },
-          -- Define custom destination patterns so icons can quickly inform you of what a link
-          -- contains. Applies to 'inline_link' and wikilink nodes.
-          -- Can specify as many additional values as you like following the 'web' pattern below
-          --   The key in this case 'web' is for healthcheck and to allow users to change its values
-          --   'pattern':   Matched against the destination text see :h lua-pattern
-          --   'icon':      Gets inlined before the link text
-          --   'highlight': Highlight for the 'icon'
-          custom = {
-            web = { pattern = "^http[s]?://", icon = "󰖟 ", highlight = "RenderMarkdownLink" },
-          },
-        },
-        callout = {
-          note = { raw = "[!NOTE]", rendered = "󰋽 Note", highlight = "RenderMarkdownInfo" },
-          tip = { raw = "[!TIP]", rendered = "󰌶 Tip", highlight = "RenderMarkdownSuccess" },
-          important = { raw = "[!IMPORTANT]", rendered = "󰅾 Important", highlight = "Identifier" },
-          warning = { raw = "[!WARNING]", rendered = "󰀪 Warning", highlight = "RenderMarkdownWarn" },
-          caution = { raw = "[!CAUTION]", rendered = "󰳦 Caution", highlight = "RenderMarkdownError" },
-          -- Obsidian: https://help.a.md/Editing+and+formatting/Callouts
-          abstract = { raw = "[!ABSTRACT]", rendered = "󰨸 Abstract", highlight = "RenderMarkdownInfo" },
-          todo = { raw = "[!TODO]", rendered = "󰗡 Todo", highlight = "RenderMarkdownInfo" },
-          success = { raw = "[!SUCCESS]", rendered = "󰄬 Success", highlight = "RenderMarkdownSuccess" },
-          question = { raw = "[!QUESTION]", rendered = "󰘥 Question", highlight = "RenderMarkdownWarn" },
-          failure = { raw = "[!FAILURE]", rendered = "󰅖 Failure", highlight = "RenderMarkdownError" },
-          danger = { raw = "[!DANGER]", rendered = "󱐌 Danger", highlight = "RenderMarkdownError" },
-          bug = { raw = "[!BUG]", rendered = "󰨰 Bug", highlight = "RenderMarkdownError" },
-          example = { raw = "[!EXAMPLE]", rendered = "󰉹 Example", highlight = "RenderMarkdownHint" },
-          quote = { raw = "[!QUOTE]", rendered = "󱆨 Quote", highlight = "RenderMarkdownQuote" },
-        },
-      }
+      require("user.config.plugins.MarkdownNvim").setup()
     end,
   },
   { "dhruvasagar/vim-table-mode" },
@@ -2195,6 +2096,9 @@ require("lazy").setup({
   {
     "nvimtools/hydra.nvim",
     event = "VeryLazy", -- 只有你真的用 hydra 的時候才會拖一點
+    config = function()
+      require("user.keymappings.hydra").setup()
+    end,
   },
 }, {
   ui = {
